400add6fb98491eff1cbe4ec31787379e96dd10a
Bodo <mbodini@cerebotani.it>
2024-03-25 18:38:08 +0100

    inizio

--- api/db.php
+++ api/db.php
@@ -1,2 +1,14 @@
 <?php
 //QUERY
+
+$db = new PDO('sqlite:gasDB');
+
+function get_users(){
+    global $db;
+
+    $sql = "SELECT * FROM utenti";
+    $stmt = $db -> prepare($sql);
+    $stmt -> execute();
+    $users = $stmt -> fetchAll(PDO::FETCH_ASSOC); //fetchAll se ottengo più righe dalla query
+    return $users;
+}
--- api/index.php
+++ api/index.php
@@ -1,2 +1,26 @@
 <?php
 //ROUTE
+
+header('Content-type: application/json; charset=UTF-8');
+
+$f3 = require('lib/base.php');
+require_once 'db.php';
+
+$f3 -> route( 'GET /users',
+    function ($f3, $params){
+        echo json_encode([
+                'result' => true,
+                'data' => get_users(),
+                'msg' => 'elenco utenti'
+        ]);
+    }
+);
+
+$f3->route(
+    'GET /*',
+    function () {
+        echo 'I am the catch em all!';
+    }
+);
+
+$f3->run();
